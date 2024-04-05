a832c3a5433d7ae7667852ddcd61223d67bda215
Dariusz Okoński <darek200180@gmail.com>
2024-04-05 22:05:14 +0200

    working on useState

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,21 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h4>You clicked: {count}</h4>
+      <button className='btn' type='button' onClick={handleClick}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleClearList = () => {
+    setPeople([]);
+  };
+
+  const handleRemoveSingle = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  const handleLoadPeople = () => {
+    setPeople(data);
+  };
+
+  return (
+    <div>
+      <h2>People</h2>
+      <ul>
+        {people.map(({ id, name }) => (
+          <li key={id}>
+            <span>{name} </span>
+            <button type='button' onClick={() => handleRemoveSingle(id)}>
+              remove me
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button className='btn' type='button' onClick={handleClearList}>
+        clear list
+      </button>
+      <button className='btn' type='button' onClick={handleLoadPeople}>
+        load people
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
