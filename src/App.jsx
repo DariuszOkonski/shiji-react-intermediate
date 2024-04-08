4a1ede0642671112d0faf386b213e28d0b20f60e
Dariusz Okoński <darek200180@gmail.com>
2024-04-08 22:23:57 +0200

    form setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-// import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
-// import Starter from './tutorial/05-leverage-javascript/starter';
-
-import List from './tutorial/05-leverage-javascript/starter/List';
+import Starter from './tutorial/06-forms/starter/01-controlled-inputs';
 
 function App() {
   return (
     <div className='container'>
-      <h2>Leverage Javascript</h2>
-      <List />
+      <Starter />
     </div>
   );
 }
--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,32 @@
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  return (
+    <form className='form'>
+      <h4>Controlled input</h4>
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          Name
+        </label>
+        <input type='text' id='name' className='form-input' />
+      </div>
+
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          Email
+        </label>
+        <input type='email' name='' id='email' className='form-input' />
+      </div>
+
+      <div className='form-row'>
+        <label htmlFor='password' className='form-label'>
+          Password
+        </label>
+        <input type='password' name='' id='password' className='form-input' />
+      </div>
+
+      <button type='submit' className='btn btn-block'>
+        submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;
