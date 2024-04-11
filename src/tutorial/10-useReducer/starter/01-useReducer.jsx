0eed417bab449a5e7d7d40bf6f4604517091c6d0
Dariusz Okoński <darek200180@gmail.com>
2024-04-11 20:05:23 +0200

    use useReducer

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useReducer } from 'react';
+import { data, people } from '../../../data';
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'REMOVE_ITEM':
+      return {
+        ...state,
+        people: state.people.filter((person) => person.id !== action.payload),
+      };
+    case 'CLEAR_LIST':
+      return {
+        ...state,
+        people: [],
+      };
+    case 'RESET_LIST':
+      return {
+        ...state,
+        people: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({ type: 'REMOVE_ITEM', payload: id });
   };
 
   const clearList = () => {
-    setPeople([]);
+    dispatch({ type: 'CLEAR_LIST' });
   };
 
   const resetList = () => {
-    setPeople(data);
+    dispatch({ type: 'RESET_LIST', payload: data });
   };
 
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -29,7 +54,7 @@ const ReducerBasics = () => {
         );
       })}
 
-      {people.length < 1 ? (
+      {state.people.length < 1 ? (
         <button
           className='btn'
           style={{ marginTop: '2rem' }}
