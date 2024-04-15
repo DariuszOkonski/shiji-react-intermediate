2299ca9a35f7c9e13dfc1b53088ced9729c695fa
Dariusz Okoński <darek200180@gmail.com>
2024-04-15 20:35:22 +0200

    lazy loading

--- src/tutorial/11-performance/starter/04-react-18/SlowComponent.jsx
+++ src/tutorial/11-performance/starter/04-react-18/SlowComponent.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const newItems = Array.from({ length: 5000 }, (_, index) => {
+const newItems = Array.from({ length: 10000 }, (_, index) => {
   return (
     <div key={index}>
       <img src='/vite.svg' alt='' />
